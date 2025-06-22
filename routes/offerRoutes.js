a5e620161ae148a04b08a21abdc14ceb2278d033
B khushal <khushalprasad242@gmail.com>
2025-06-22 17:46:19 +0530

    Promocodes popup

--- routes/offerRoutes.js
+++ routes/offerRoutes.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { authMiddleware, isAdmin } = require('../middleware/authMiddleware');
+const { protect, admin } = require('../middleware/authMiddleware');
+
+// Debug logging
+console.log('Loading offerRoutes.js');
+
 const {
   getActiveOffers,
   getAllOffers,
@@ -10,14 +14,24 @@ const {
   toggleOfferStatus
 } = require('../controllers/offerController');
 
+// Debug logging for controller functions
+console.log('Controller functions loaded:', {
+  getActiveOffers: typeof getActiveOffers,
+  getAllOffers: typeof getAllOffers,
+  createOffer: typeof createOffer,
+  updateOffer: typeof updateOffer,
+  deleteOffer: typeof deleteOffer,
+  toggleOfferStatus: typeof toggleOfferStatus
+});
+
 // Public routes
 router.get('/active', getActiveOffers);
 
 // Admin routes (protected)
-router.get('/all', authMiddleware, isAdmin, getAllOffers);
-router.post('/', authMiddleware, isAdmin, createOffer);
-router.put('/:id', authMiddleware, isAdmin, updateOffer);
-router.delete('/:id', authMiddleware, isAdmin, deleteOffer);
-router.patch('/:id/toggle', authMiddleware, isAdmin, toggleOfferStatus);
+router.get('/all', protect, admin, getAllOffers);
+router.post('/', protect, admin, createOffer);
+router.put('/:id', protect, admin, updateOffer);
+router.delete('/:id', protect, admin, deleteOffer);
+router.patch('/:id/toggle', protect, admin, toggleOfferStatus);
 
 module.exports = router; 
