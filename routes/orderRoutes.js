5f2d6e6e076647726d0c83e49103fe1633bb77d5
B khushal <khushalprasad242@gmail.com>
2025-07-07 15:00:20 +0530

    payment

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -10,7 +10,7 @@ const {
   updateOrderToDelivered,
   updateOrderStatus,
   getNextOrderNumber,
-  createRazorpayOrder,
+  createRazorpayOrderHandler,
   verifyRazorpayPayment,
   getUpcomingDeliveries,
   getDeliveryCalendar,
