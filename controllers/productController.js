0bdef7f6b6d8a510d9932ef670e3438065e85402
B khushal <khushalprasad242@gmail.com>
2025-07-07 19:30:13 +0530

    payment

--- controllers/productController.js
+++ controllers/productController.js
@@ -868,6 +868,10 @@ const removeFromCart = async (req, res) => {
     );
     
     await user.save();
+    
+    // Populate product details for response
+    await user.populate('cart.productId');
+    
     res.status(200).json({ 
       message: "Product removed from cart",
       cart: user.cart
@@ -909,6 +913,10 @@ const updateCartQuantity = async (req, res) => {
     }
     
     await user.save();
+    
+    // Populate product details for response
+    await user.populate('cart.productId');
+    
     res.status(200).json({ 
       message: "Cart updated",
       cart: user.cart
